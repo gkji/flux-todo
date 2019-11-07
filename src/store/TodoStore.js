829e84482437ad8b8f2e1349c23a7c975f86e7ec
qinghe <iwangqinghe@gmail.com>
2019-11-07 11:49:08 +0800

    flux-todo 初始化

---
+++ src/store/TodoStore.js
@@ -0,0 +1,71 @@
+
+
+import Immutable from 'immutable'
+import {ReduceStore} from 'flux/utils'
+import ationType from '../action/acitonType'
+import dispatcher from '../dispatcher'
+
+const Todo = Immutable.Record({
+  id: '',
+  complete: false,
+  text: '',
+});
+
+
+
+let _counter = 1;
+const Counter = {
+  increment() {
+    return 'id-' + String(_counter++);
+  },
+};
+
+class TodoStore extends ReduceStore {
+  constructor() {
+    super(dispatcher);
+  }
+
+  getInitialState() {
+    return Immutable.OrderedMap();
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case ationType.addTodo:
+        // Don't add todos with no text.
+        if (!action.text) {
+          return state;
+        }
+        const id = Counter.increment();
+        return state.set(id, new Todo({
+          id,
+          text: action.text,
+          complete: false,
+        }));
+
+      case ationType.deleteCompletedTodo:
+        return state.filter(todo => !todo.complete);
+
+      case ationType.deleteTodo:
+        return state.delete(action.id);
+
+      case ationType.editTodo:
+        return state.setIn([action.id, 'text'], action.text);
+
+      case ationType.toggleAllTodo:
+        const areAllComplete = state.every(todo => todo.complete);
+        return state.map(todo => todo.set('complete', !areAllComplete));
+
+      case ationType.toggleTodo:
+        return state.update(
+          action.id,
+          todo => todo.set('complete', !todo.complete),
+        );
+
+      default:
+        return state;
+    }
+  }
+}
+
+export default new TodoStore();
